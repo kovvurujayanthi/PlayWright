80b9a9e05e928d083acca290ea6597f3355a0165
VIJAY\polanki <vijay.polanki1996@gmail.com>
2026-04-04 21:16:20 +0530

    Initial commit - JavaScript practice

---
+++ Basicexample.js
@@ -0,0 +1,15 @@
+// console.log("Hello Jayanthi ");
+
+// let a = 10;
+// const b = 20;
+
+// console.log("Sum is:", a + b);
+// console.log("Diff is:", a-b);
+
+let a = 50;
+let b = 20;
+
+console.log("Sum is:", a + b);
+console.log("Diff is:", a - b);
+console.log("Multiply:", a * b);
+console.log("Divide:", a / b);
