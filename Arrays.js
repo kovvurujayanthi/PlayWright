e11de9e04ebffa73908ee8a89069b0f205631d4b
VIJAY\polanki <vijay.polanki1996@gmail.com>
2026-04-08 10:35:08 +0530

    updated playwright tests

---
+++ Arrays.js
@@ -0,0 +1,31 @@
+// const lists = ["User1", "user2", "user3"];
+
+// console.log(lists);
+
+// const users = ["user1", "user2", "admin"];
+
+// if (users.includes("admin1")) {
+//   console.log("Admin user exists");
+// } else {
+//   console.log("Admin not found");
+// }
+
+// const users = ["admin", "user1", "user2"];
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log("Checking user:", users[i]);
+// }
+// Adding value in array 
+
+// const users = ["admin", "user1"];
+
+// users.push("newUser");
+
+// console.log(users);
+
+// Deleting value in array - pop removes last element of array
+const users = ["admin", "user1", "user2"];
+
+users.pop();
+
+console.log(users);
